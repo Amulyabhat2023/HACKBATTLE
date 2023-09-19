41d0674ccbb75f11f56e6ac798901deb85292870
Amulyabhat2023 <145366264+Amulyabhat2023@users.noreply.github.com>
2023-09-20 02:47:53 +0530

    Add files via upload

---
+++ Hackbattle.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(page_title="VITFreshie!", page_icon=":tada:", layout= "wide")
+
+with st.container():
+    st.subheader("Hello freshers :wave:")
+    st.subheader("Welcome to VIT!")
+    st.title("A peep into Vit")
+    st.write("The VITfreshie app helps freshers feel at home.")
+
+
+with st.container():
+    st.write("---")
+    st.header("Features")
+    st.write("##")
+    st.write("No idea how to find your way around VIT?  "
+            "[Get access to the VIT map](https://www.mirlabs.org/socpar16/venue_clip_image002.jpg)")
+    st.write("Want to know the question paper pattern of CAT and FAT?   "
+            "[A collection of PYPs](https://drive.google.com/drive/folders/1ft579_REKlGjp6HDkkgIhH55W1ljJKVm?usp=drive_link)")
+    st.write("Lot of queries troubling your mind?  "
+             "Talk to your seniors and clear your doubts:")
+    st.write("Get to know your roommates and students from your region"
+             "Find my mate:")
+    st.write("Be hostel ready with a full list of Hostel Essentials:  "
+             "[Things to carry](https://thenomadicpanda.files.wordpress.com/2019/08/hostel-packing-list-20-essentials-you-have-to-take-e1565864843733.png)")
+
+
+
+
+
